0f83d4f1e5c2609ecf9db5e8b5168485c3c6a1fa
TheRayGent <109617962+TheRayGent@users.noreply.github.com>
2026-06-04 03:00:46 +0200

    update #header, #sources and #footer

--- Custom Tape Operator.user.js
+++ Custom Tape Operator.user.js
@@ -1,18 +1,35 @@
 // ==UserScript==
 // @name         Custom Tape Operator
 // @icon         https://tapeop.dev/assets/favicon.png
-// @version      0.0.1
+// @version      0.0.2
 // @match        https://tapeop.dev/*
 // ==/UserScript==
 
 (function() {
     'use strict';
+    const logo = document.querySelector('#logo');
+    if (logo) {
+        logo.remove();
+    }
     const style = document.createElement('style');
     style.textContent = `
   @media screen and (max-width: 800px), screen and (max-height: 600px) {
     #container {
       padding-left: 0 !important;
       padding-right: 0 !important;
+    }
+
+    #header,
+    #sources,
+    #footer {
+      font-size: 0.9rem;
+      height: 2.7rem;
+      min-height: 2.7rem;
+      line-height: 2.7rem;
+    }
+
+    #watched-movies-toggle {
+      font-size: 0.9rem;
     }
   }
 `;
